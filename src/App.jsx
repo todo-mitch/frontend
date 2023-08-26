c2ee1857ce7644fdbf387aca7d99a0c785609378
parari415 <parari415@gmail.com>
2023-08-27 07:37:19 +0900

    thirdcommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-
-
 import React,{ useState, useEffect } from 'react';
 
 function App() {
@@ -12,7 +10,7 @@ function App() {
   }, []);
 
   const fetchData = () => {
-    fetch('/get')
+    fetch('/tasks')
       .then(response => response.json())
       .then(data => {
         setData(data);
@@ -23,7 +21,7 @@ function App() {
   };
 
   const handlePost = () => {
-    fetch('/post', {
+    fetch('/tasks', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -41,7 +39,7 @@ function App() {
   };
 
   const handleEdit = (id, newContent) => {
-    fetch('/edit', {
+    fetch('/tasks/{task_id}', {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json'
@@ -59,26 +57,26 @@ function App() {
       });
   };
 
-  const handleTempDelete = (id) => {
-    fetch(`/edit`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(id)
-    })
-      .then(response => response.json())
-      .then(tempDeletedItem => {
-        console.log('一時削除結果:', tempDeletedItem);
-        fetchData();
-      })
-      .catch(error => {
-        console.error('一時削除エラー:', error);
-      });
-  };
+  // const handleTempDelete = (id) => {
+  //   fetch(`/tasks/{task_id}/done`, {
+  //     method: 'PATCH',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify(id)
+  //   })
+  //     .then(response => response.json())
+  //     .then(tempDeletedItem => {
+  //       console.log('一時削除結果:', tempDeletedItem);
+  //       fetchData();
+  //     })
+  //     .catch(error => {
+  //       console.error('一時削除エラー:', error);
+  //     });
+  // };
 
   const handleDelete = (id) => {
-    fetch(`/delete`, {
+    fetch(`/tasks/{task_id}`, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json'
@@ -96,13 +94,22 @@ function App() {
   };
   return (
     <div>
-      <h1>ToDO</h1>
+      <h1>todo-mitch</h1>
+      <div>
+        <h2>タスク追加</h2>
+        <input
+          type="text"
+          value={content}
+          onChange={event => setContent(event.target.value)}
+        />
+        <button onClick={handlePost}>追加</button>
+      </div>
       <ul>
         {data.map(item => (
           <li key={item.id}>
             {item.content}
             <button onClick={() => setEditId(item.id)}>編集</button>
-            <button onClick={() => handleTempDelete(item.id)}>一時削除</button>
+            {/* <button onClick={() => handleTempDelete(item.id)}>一時削除</button> */}
             <button onClick={() => handleDelete(item.id)}>完全削除</button>
             {editId === item.id && (
               <div>
@@ -117,15 +124,6 @@ function App() {
           </li>
         ))}
       </ul>
-      <div>
-        <h2>投稿する内容</h2>
-        <input
-          type="text"
-          value={content}
-          onChange={event => setContent(event.target.value)}
-        />
-        <button onClick={handlePost}>投稿</button>
-      </div>
     </div>
   );
   }
